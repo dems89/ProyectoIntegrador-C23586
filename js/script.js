8b9dc9d9aa49a1138ab87ef5afb92593ca63bd47
dems89 <demianscarafiocca@gmail.com>
2023-11-01 23:22:27 -0300

    /Mejoras en validaciones JS/

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const btnborrar = document.getElementById("borrar");
-  btnborrar.addEventListener("click", () => {
-    document.getElementById("name").value = "";
-    document.getElementById("lastName").value = "";
-    document.getElementById("mail").value = "";
-    document.getElementById("categoria").value = "";
-    document.getElementById("sumatoria").innerHTML = "";
-    document.getElementById("cantidad").value = "";
-    /*document.getElementById("formulario").classList="";*/
-  });
 
   const categorizar = (categoria) => {
     switch (categoria) {
@@ -18,55 +8,81 @@ document.addEventListener("DOMContentLoaded", () => {
         return 50;
       case "3":
         return 15;
-        case "4":
+      case "4":
         return 0;
       default:
         return 0;
     }
   };
-
+  
   const validarNumero = (numero) => {
-    return numero > 0;
+    if (numero.value > 0) {
+      numero.classList.remove("is-invalid");
+      numero.classList.add("is-valid");
+      return parseInt(numero.value);
+    } else {
+      numero.classList.remove("is-valid");
+      numero.classList.add("is-invalid");
+      numero.focus();
+    }
   };
 
   const validarTexto = (texto) => {
-    return texto.value.trim();
+    if (isNaN(texto.value)) {
+      texto.classList.remove("is-invalid");
+      texto.classList.add("is-valid");
+      return texto.value;
+    } else {
+      texto.classList.remove("is-valid");
+      texto.classList.add("is-invalid");
+      texto.focus();
+    }
   };
 
   const validarEmail = (email) => {
-    const esMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]/;
-    //const esMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return esMail.test(email);
+    const esMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (esMail.test(email.value)) {
+      email.classList.remove("is-invalid");
+      email.classList.add("is-valid");
+      return email;
+    } else {
+      email.classList.remove("is-valid");
+      email.classList.add("is-invalid");
+      email.focus();
+    }
   };
-
- 
   const btnresumen = document.getElementById("calcular");
-
   btnresumen.addEventListener("click", () => {
     let nameInput = document.getElementById("name");
     let lastNameInput = document.getElementById("lastName");
     let mailInput = document.getElementById("mail");
     let categoriaInput = document.getElementById("categoria");
-    let sumatoria = document.getElementById("sumatoria");
+    let totalPagar = document.getElementById("total-pagar");
     let cantidadInput = document.getElementById("cantidad");
-    /*let formulario = document.getElementById("formulario").classList;
-    formulario.add("was-validated");*/
-    
 
     if (validarTexto(nameInput)) {
       if (validarTexto(lastNameInput)) {
-        if (validarEmail(mailInput.value)) {
+        if (validarEmail(mailInput)) {
           const precio = 200;
           let cat = categoriaInput.value;
           let descuento = categorizar(cat);
-          let cant = parseInt(cantidadInput.value);
 
-          if (validarNumero(cant)) {
-            let precioFinal = (precio - (precio * descuento) / 100) * cant;
-            sumatoria.innerHTML = "$"+precioFinal;
-          } 
-        } 
-      } 
-    } 
+          if (validarNumero(cantidadInput)) {
+            let precioFinal =
+              (precio - (precio * descuento) / 100) * cantidadInput.value;
+            totalPagar.innerHTML = "$" + precioFinal;
+          }
+        }
+      }
+    }
+  });
+  const botonBorrar = document.getElementById("borrar");
+  botonBorrar.addEventListener("click", () => {
+    const formulario = document.getElementById("formulario");
+    const campos = formulario.querySelectorAll("input");
+    document.getElementById("total-pagar").innerHTML = "";
+    campos.forEach((campo) => {
+      campo.classList.remove("is-valid", "is-invalid");
+    });
   });
 });
